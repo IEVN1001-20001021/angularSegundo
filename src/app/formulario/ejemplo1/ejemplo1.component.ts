323eafd124c3cb675faeae617dd48ae9f7bc18fa
Miguel Rumbo <miguelrumbogames@gmail.com>
2024-10-14 20:59:33 -0600

    Segundo Commit Rutas Lunes 14

--- src/app/formulario/ejemplo1/ejemplo1.component.ts
+++ src/app/formulario/ejemplo1/ejemplo1.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
+interface Usuario{
+  nombre: string;
+  edad: number;
+  email: string;
+}
 @Component({
   selector: 'app-ejemplo1',
   standalone: true,
@@ -10,6 +15,13 @@ import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class Ejemplo1Component implements OnInit{
   formGroup!: FormGroup;
+  nombre: string = 'Miguel';
+
+  persona: Usuario = {
+    nombre: 'Juan',
+    edad: 25,
+    email: 'miguelrumbo24@gmail.com'
+  }
 
   constructor(private readonly fb: FormBuilder) {}
 
@@ -19,12 +31,28 @@ export class Ejemplo1Component implements OnInit{
 
   initForm(): FormGroup{
     return this.fb.group({
-      email: [''],
-      password: ['']
+      nombre: [''],
+      edad : [''],
+      email: ['']
     })
   }
 
   onSubmit():void{
-    console.log('Form ->', this.formGroup.value);
+    const {nombre, edad, email} = this.formGroup.value;
+    this.persona.nombre = nombre
+    this.persona.edad = edad
+    this.persona.email = email
+
+    let personaJSON = JSON.stringify(this.persona)
+    
+    localStorage.setItem("persona", personaJSON);
+  }
+
+  subImprimir():void{
+    const usuarioGuardado = localStorage.getItem("persona");
+    if (usuarioGuardado){
+      const usuarioRecuperado: Usuario = JSON.parse(usuarioGuardado);
+      this.persona = usuarioRecuperado;
+    }
   }
 }
